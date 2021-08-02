36ff75639ebf02508341e6a62201cbd6d0d3967e
Gilqamesh <celicaw8@gmail.com>
2021-08-02 17:49:52 +0200

    Update ft_itobase.c

--- libft/ft_itobase.c
+++ libft/ft_itobase.c
@@ -1,40 +1,31 @@
 #include "libft.h"
 #include <stdlib.h>
 
-static int	itobase_helper(char **result, int len, int negative)
+static int	itobase_helper(char **result, int len)
 {
-	*result = malloc(len + negative + 1);
+	*result = malloc(len + 1);
 	if (!*result)
 		return (0);
-	(*result)[len + negative] = '\0';
-	if (negative)
-		**result = '-';
+	(*result)[len] = '\0';
 	return (1);
 }
 
-char	*ft_itobase(int64_t n, int base)
+char	*ft_itobase(unsigned long n, int base)
 {
-	int		negative;
 	int		len;
 	char	*result;
 
 	if (base < 2)
 		return ((char *)0);
-	negative = 0;
-	if (n < 0)
-	{
-		negative = 1;
-		n *= -1;
-	}
 	len = ft_baselen(n, base);
-	if (!itobase_helper(&result, len, negative))
+	if (!itobase_helper(&result, len))
 		return ((char *)0);
 	while (len-- > 0)
 	{
 		if (n % base > 9)
-			result[len + negative] = n % base - 10 + 'a';
+			result[len] = n % base - 10 + 'a';
 		else
-			result[len + negative] = n % base + '0';
+			result[len] = n % base + '0';
 		n /= base;
 	}
 	return (result);
