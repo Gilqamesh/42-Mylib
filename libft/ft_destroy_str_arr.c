e6480bbc9f60b077ee6bbff3a0ce86fd9b5c5821
Erchegyi David <edavid@2-G-3.42heilbronn.de>
2021-08-24 16:54:12 +0200

    update libft/ft_destroy_str_arr.c

--- libft/ft_destroy_str_arr.c
+++ libft/ft_destroy_str_arr.c
@@ -6,7 +6,7 @@
 /*   By: edavid <edavid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/03 18:37:05 by edavid            #+#    #+#             */
-/*   Updated: 2021/08/03 18:38:36 by edavid           ###   ########.fr       */
+/*   Updated: 2021/08/23 19:48:29 by edavid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	ft_destroy_str_arr(char ***str)
 {
 	int	i;
 
+	if (str == NULL || *str == NULL)
+		return ;
 	i = -1;
 	while ((*str)[++i])
 		free((*str)[i]);
