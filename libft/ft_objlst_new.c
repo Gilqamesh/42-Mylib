7b60d6415d2b15f249d7add0a0cd73035b78b1a4
Gilqamesh <celicaw8@gmail.com>
2021-09-10 18:20:32 +0200

    Update ft_objlst_new.c

--- libft/ft_objlst_new.c
+++ libft/ft_objlst_new.c
@@ -6,7 +6,7 @@
 /*   By: edavid <edavid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 17:55:29 by edavid            #+#    #+#             */
-/*   Updated: 2021/09/10 18:01:04 by edavid           ###   ########.fr       */
+/*   Updated: 2021/09/10 18:20:13 by edavid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 /*
 ** Allocates and returns a new element t_obj_lst *result;
-** result->key = ft_strdup(key);
-** result->value = ft_strdup(value);
+** result->key = key;
+** result->value = value;
 ** The variable 'next' is initialized to NULL.
 */
 t_obj_lst	*ft_objlst_new(char *key, char *value)
@@ -24,10 +24,8 @@ t_obj_lst	*ft_objlst_new(char *key, char *value)
 	t_obj_lst	*new;
 
 	new = ft_calloc(1, sizeof(*new));
-	if (key)
-		new->key = ft_strdup(key);
-	if (value)
-		new->value = ft_strdup(value);
+	new->key = key;
+	new->value = value;
 	new->next = NULL;
 	return (new);
 }
