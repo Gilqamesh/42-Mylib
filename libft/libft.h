76a4ca6e9eba33667f59b47642679133ad8eeb3f
Gilqamesh <celicaw8@gmail.com>
2021-09-28 17:03:25 +0200

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -397,5 +397,9 @@ char			**ft_strToStrArr(char *str);
 // ex of valid strings: "-234", "+3432"
 // ex of not valid strings: "--32", "234a"
 bool			ft_isStrNumber(char *str);
+// Install 'nullable_ptr' by passing it as an argument first time calling this
+// function. Anytime the function called afterwards it will return the installed
+// pointer.
+void			*ft_get_ptr(void *nullable_ptr);
 
 #endif
